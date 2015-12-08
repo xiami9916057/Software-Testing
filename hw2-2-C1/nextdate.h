974b8baac233ef0476881a81d9af943e135d80a8
xiami9916057 <xiami9916057@gmail.com>
2015-12-09 01:26:06 +0800

    Update Hw2 nextdate c1

---
+++ hw2-2-C1/nextdate.h
@@ -0,0 +1,14 @@
+
+#include <string>
+
+using namespace std;
+
+
+#ifndef GTEST_NEXTDATE_H_
+#define GTEST_NEXTDATE_H_
+
+string NextDate(int month,int day,int year);
+bool IsLeapYear(int year);
+
+
+#endif  // GTEST_NEXTDATE_H_

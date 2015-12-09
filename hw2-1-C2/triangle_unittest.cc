fc921cf3f17b4f4ebce026b7f347d8311b2d0d2c
xiami9916057 <xiami9916057@gmail.com>
2015-12-09 22:16:54 +0800

    Update Hw2 triangle c2

---
+++ hw2-1-C2/triangle_unittest.cc
@@ -0,0 +1,77 @@
+// Step 1. Include necessary header files such that the stuff your
+// test logic needs is declared.
+//
+// Don't forget gtest.h, which declares the testing framework.
+
+
+#include "triangle.h"
+#include "gtest/gtest.h"
+#include <string>
+
+using namespace std;
+
+// Step 2. Use the TEST macro to define your tests.
+//
+// TEST has two parameters: the test case name and the test name.
+// After using the macro, you should define your test logic between a
+// pair of braces.  You can use a bunch of macros to indicate the
+// success or failure of a test.  EXPECT_TRUE and EXPECT_EQ are
+// examples of such macros.  For a complete list, see gtest.h.
+
+
+
+TEST(TriangleTest, SimpleLoopCoverage){
+
+	//                    | 1 2 3 4 5 6 7 8                                    
+	//c1:a<b+c?           | T T T T F F F F     
+	//c2:b<a+c?           | T T F F T T F F
+	//c3:c<a+b?           | T F T F T F T F
+
+	/* 01 */EXPECT_STREQ("Isosceles",Triangle(10,10,15).c_str());
+	/* 02 */EXPECT_STREQ("Not a Triangle",Triangle(10,10,20).c_str());
+	/* 03 */EXPECT_STREQ("Not a Triangle",Triangle(10,20,10).c_str());
+	/* 04   EXPECT_STREQ("Impossible",Triangle(?,?,?).c_str());*/
+	/* 05 */EXPECT_STREQ("Not a Triangle",Triangle(20,10,10).c_str());
+	/* 06   EXPECT_STREQ("Impossible",Triangle(?,?,?).c_str());*/
+	/* 07   EXPECT_STREQ("Impossible",Triangle(?,?,?).c_str());*/
+	/* 08   EXPECT_STREQ("Impossible",Triangle(?,?,?).c_str());*/
+
+	//                    | 1 2 3 4
+	//c4:a==b?            | T T F F
+	//c5:b==c?            | T F T F
+
+	/* 01 */EXPECT_STREQ("Equilateral",Triangle(10,10,10).c_str());
+	/* 02 */EXPECT_STREQ("Isosceles",Triangle(10,10,15).c_str());
+	/* 03 */EXPECT_STREQ("Isosceles",Triangle(10,15,15).c_str());
+	/* 04 */EXPECT_STREQ("Scalene",Triangle(10,15,20).c_str());
+
+	//                    | 1 2 3 4 5 6 7 8 
+	//c6:a!=b?            | T T T T F F F F 
+	//c7:a!=c?            | T T F F T T F F
+	//c8:b!=c?            | T F T F T F T F
+
+	/* 01 */EXPECT_STREQ("Scalene",Triangle(10,15,20).c_str());
+	/* 02 */EXPECT_STREQ("Isosceles",Triangle(15,10,10).c_str());
+	/* 03 */EXPECT_STREQ("Isosceles",Triangle(10,15,10).c_str());
+	/* 04   EXPECT_STREQ("Impossible",Triangle(?,?,?).c_str());*/
+	/* 05 */EXPECT_STREQ("Isosceles",Triangle(10,10,15).c_str());
+	/* 06   EXPECT_STREQ("Impossible",Triangle(?,?,?).c_str());*/
+	/* 07   EXPECT_STREQ("Impossible",Triangle(?,?,?).c_str());*/
+	/* 08 */EXPECT_STREQ("Equilateral",Triangle(10,10,10).c_str());
+} 
+
+
+
+
+// Step 3. Call RUN_ALL_TESTS() in main().
+//
+// We do this by linking in src/gtest_main.cc file, which consists of
+// a main() function which calls RUN_ALL_TESTS() for us.
+//
+// This runs all the tests you've defined, prints the result, and
+// returns 0 if successful, or 1 otherwise.
+//
+// Did you notice that we didn't register the tests?  The
+// RUN_ALL_TESTS() macro magically knows about all the tests we
+// defined.  Isn't this convenient?
+
